cc847d935fbfcc39471b7bfe2d1c44a41272b4ac
teramura0622 <95912639+teramura0622@users.noreply.github.com>
2022-01-21 18:58:03 +0900

    Teramura
    test

---
+++ Assets/Scripts/ExplosionAction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionAction : MonoBehaviour {
+
+    ParticleSystem myPat;
+
+    void Start() {
+        myPat = GetComponent<ParticleSystem>();
+    }
+
+    void Update() {
+        if (myPat) {
+            if (!myPat.isPlaying) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
